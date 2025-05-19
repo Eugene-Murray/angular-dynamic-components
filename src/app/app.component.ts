6cba556923e19f482f92aa69330d846f06f69a8d
Eugene Murray <eugene.murray@gmail.com>
2025-05-19 18:12:19 +0100

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgComponentOutlet } from '@angular/common';
+import { Component, inject, ViewContainerRef } from '@angular/core';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgComponentOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-dynamic-components';
+  dynamicComponent = Component1Component;
+  lazyLoadComponent: any = null;
+
+  private viewContainer = inject(ViewContainerRef);
+
+  onloadComponentViewContainer() {
+    this.viewContainer.createComponent(Component2Component);
+  }
+
+  async onlazyLoadComponent() {
+    const { Component3Component } = await import('./components/component3/component3.component');
+    this.lazyLoadComponent = Component3Component;
+  }
+
 }
